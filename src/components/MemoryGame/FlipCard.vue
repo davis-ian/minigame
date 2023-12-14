2cd0fc00a8335483e207adb4488b1d722c9f4d0d
ian <iandavis6481@gmail.com>
2023-12-14 07:10:28 -0600

    responsive font-size

--- src/components/MemoryGame/FlipCard.vue
+++ src/components/MemoryGame/FlipCard.vue
@@ -8,7 +8,9 @@ defineProps({
 </script>
 
 <template>
-	<Card class="grid-item text-center text-7xl border-round-md font-bold border-3 shadow-3 pointer">
+	<Card
+		class="grid-item text-center md:text-7xl border-round-md font-bold border-3 shadow-3 pointer"
+	>
 		<template #content>
 			<span v-if="displayValue">{{ item.icon }}</span>
 			<span v-else>❔</span>
